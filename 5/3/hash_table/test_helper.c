3002013257131e2c142276296aed33382e80911e
tentstan <tentstanhailin.tentstan@gmail.com>
2019-03-01 12:42:09 +0800

    hash_table implement

---
+++ 5/3/hash_table/test_helper.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_helper.c
+ *
+ *    Description:  test_helper c source file 
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 03:41:03 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+static int hash_int(void* data)
+{
+    return (int)data;
+}
+
+static int cmp_int(void* ctx,void* data)
+{
+    return (int)ctx - (int)data;
+}
+
+static Ret print_int(void* ctx,void* data)
+{
+    printf("%d ",(int)data);
+
+    return RET_OK;
+}
