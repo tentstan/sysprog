f462150aba8ada1d91a9c0cd7971f05663d66b36
tentstan <tentstanhailin.tentstan@gmail.com>
2019-04-01 17:12:35 +0800

    add ignore file and surport automake

---
+++ 7/2/helloworld/src/main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  main c source file 
+ *
+ *        Version:  1.0
+ *        Created:  04/01/2019 05:01:48 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc,char* argv[])
+{
+    printf("Hello world.\n");
+
+    return 0;
+}
